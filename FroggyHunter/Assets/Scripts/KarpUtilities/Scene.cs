77d44705fa9aca1b0208fb7f4159e93130136b28
Deleye Arthur <arthurdeleye@gmail.com>
2020-07-04 12:10:56 +0200

    Transitions feuilles

--- FroggyHunter/Assets/Scripts/KarpUtilities/Scene.cs
+++ FroggyHunter/Assets/Scripts/KarpUtilities/Scene.cs
@@ -1,10 +1,40 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class Scene : MonoBehaviour
 {
+    [SerializeField] private string sceneToGo;
+
+    public void GoToSerializedScene()
+    {
+        SceneManager.LoadScene(sceneToGo);
+    }
+
+    public void GoToSceneIn(float time)
+    {
+        Invoke("GoToSerializedScene", time);
+    }
+
+    public void NextSceneIn(float time)
+    {
+        Invoke("NextScene", time);
+    }
+
+    public void PreviousSceneIn(float time)
+    {
+        Invoke("PreviousScene", time);
+    }
+
+    public void QuitGameIn(float time)
+    {
+        Invoke("QuitGame", time);
+    }
+
+    public void GoToScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
     public void NextScene()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
@@ -13,11 +43,6 @@ public class Scene : MonoBehaviour
     public void PreviousScene()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
-    }
-
-    public void GoToScene(string sceneName)
-    {
-        SceneManager.LoadScene(sceneName);
     }
 
     public void QuitGame()
--- FroggyHunter/Assets/Scripts/UI/Transition.cs
+++ FroggyHunter/Assets/Scripts/UI/Transition.cs
@@ -1,18 +1,30 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Transition : MonoBehaviour
 {
+    [Header("Component")]
+    [SerializeField] private RectTransform leftPart = null, rightPart = null;
 
-    void Start()
+    [Header("Paramètre")]
+    [SerializeField] bool isOpen = false;
+
+    private void Start()
     {
-        
+        if (!isOpen)
+        {
+            Open();
+        }
     }
 
-
-    void Update()
+    public void Open()
     {
-        
+        LeanTween.moveX(rightPart, 1200, 1).setEaseOutQuad();
+        LeanTween.moveX(leftPart, -1200, 1).setEaseOutQuad();
+    }
+
+    public void Close()
+    {
+        LeanTween.moveX(rightPart, 0, 1).setEaseInQuad();
+        LeanTween.moveX(leftPart,  0, 1).setEaseInQuad();
     }
 }
