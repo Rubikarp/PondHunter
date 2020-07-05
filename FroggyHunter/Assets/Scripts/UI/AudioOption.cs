bea96328a373fdf628b4931693c5a6b29650a1ff
Deleye Arthur <arthurdeleye@gmail.com>
2020-07-05 17:15:57 +0200

    Slider audio

---
+++ FroggyHunter/Assets/Scripts/UI/AudioOption.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioOption : MonoBehaviour
+{
+    [SerializeField] private AudioMixer audioMixer;
+
+    public void SetMasterVolume(float volume)
+    {
+        audioMixer.SetFloat("MasterVolume", Mathf.Log10(volume) * 20);
+    }
+    public void SetMusicVolume(float volume)
+    {
+        audioMixer.SetFloat("MusicVolume", Mathf.Log10(volume) * 20);
+    }
+    public void SetSoundEffectVolume(float volume)
+    {
+        audioMixer.SetFloat("SFXVolume", Mathf.Log10(volume) * 20);
+    }
+    public void SetAmbiantVolume(float volume)
+    {
+        audioMixer.SetFloat("AmbiantVolume", Mathf.Log10(volume) * 20);
+    }
+}
